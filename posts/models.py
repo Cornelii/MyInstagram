2fefd29d30d552e61b9632364f36b91d99f48b1f
Cornelii G Son <cornelii.g.son@gmail.com>
2019-04-08 08:18:30 +0000

    post 모델 생성 & migrate 완료

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    content = models.CharField(max_length=220)
+    
+    
+    def __str__(self):
+        return f"{self.id}: {self.content}"
