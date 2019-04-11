21036e40d553f8acf83e9eafd021489d934ccb7c
Cornelii G Son <cornelii.g.son@gmail.com>
2019-04-11 08:24:44 +0000

    User-Post 1:N 연결

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+#from django.contrib.auth.models import User 직접 접근
+from django.conf import settings
+# django.conf~~!!!!
+
 
 # Create your models here.
 class Post(models.Model):
     content = models.CharField(max_length=220)
     image = models.ImageField(blank=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
     
     def __str__(self):
         return f"{self.id}: {self.content}"
     
-    
-
