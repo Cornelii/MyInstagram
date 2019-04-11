d56eddc37a68808f5c8598418a3406a0a450e6fc
Cornelii G Son <cornelii.g.son@gmail.com>
2019-04-11 06:35:31 +0000

    업데이트 기능 추가

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,7 @@ app_name="posts"
 urlpatterns = [
         path('create/', views.create, name='create'),
         path('', views.list, name="list"),
+        path('<int:post_id>/', views.delete, name='delete'),
+        path('<int:post_id>/update/', views.update, name='update'),
         
     ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+
 from .forms import PostModelForm
 from .models import Post
 # Create your views here.
+
 
 def create(request):
     if request.method == "POST":
@@ -26,3 +29,29 @@ def list(request):
         'posts':posts
     }
     return render(request,'posts/list.html',context)
+    
+@require_POST
+def delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('posts:list')
+        
+def update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == "POST":
+        
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:list')
+        else:
+            # error handling
+            pass
+    else:
+        
+        form = PostModelForm(instance=post)
+        return render(request, 'posts/create.html', {
+            'form':form
+        })
+
+
