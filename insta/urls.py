529cd7bad01c0de3e60ab82f8b2d0f1baa642b75
Cornelii G Son <cornelii.g.son@gmail.com>
2019-04-12 08:20:06 +0000

    file_upload를 위한 static추가

--- insta/urls.py
+++ insta/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
@@ -14,3 +15,5 @@ urlpatterns = [
 # Dev only
 # static(통과하고자 하는 url, 실제 저장 장소)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
